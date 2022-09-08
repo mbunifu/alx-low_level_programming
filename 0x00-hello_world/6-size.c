95d03db6e0084665aa68f82d3f905610ba2d4df9
mbunifu <murimi.nicko@gmail.com>
2022-09-08 14:36:54 -0700

    print size of values

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 {
 printf("Size of char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d bytes(s)\n", sizeof(int));
-printf("Size of a long int: %d byte(s)", sizeof(sizeof(long int));
+printf("Size of a long int: %d byte(s)", sizeof(long int));
 printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(fload));
 return (0);
