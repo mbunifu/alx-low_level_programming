c0a3fa719ebc50d6f9ec759f9a6aa49782e641f3
mbunifu <murimi.nicko@gmail.com>
2022-09-08 14:41:22 -0700

    print size of memory of values for diferrent computers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ printf("Size of char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d bytes(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)", sizeof(long int));
 printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(fload));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
