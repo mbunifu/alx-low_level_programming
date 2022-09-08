76c970f4c7d896c746eb6a4e1e5b7d1242dbec92
mbunifu <murimi.nicko@gmail.com>
2022-09-08 14:26:24 -0700

    printf codes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h
+/**
+ *main - Entry6 point
+ *
+ *Return: Always 0 Success
+ */
+int main(void)
+{
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d bytes(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)", sizeof(sizeof(long int));
+printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(fload));
+return (0);
+}
