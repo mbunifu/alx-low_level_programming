aa41dc8ada4473fc9c3bc1699c7c5e81c20327e2
mbunifu <murimi.nicko@gmail.com>
2022-09-09 12:28:08 -0700

    if esle statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,15 @@
 #include <time.h>
 
 /**
- *more headers goes there
- *main - if else statements
- *Return: 0 (success)
- *betty style doc for function main goes there
+ *
+ * main - main block
+ *
+ * Description: Get a random number and print the number
+ *
+ * and if it is positive, negative, or zero
+ *
+ * Return: 0
+ *
  */
 int main(void)
 
