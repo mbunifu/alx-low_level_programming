70d456a79307e0a9ab9a8276932a2b5a8d703ee7
mbunifu <murimi.nicko@gmail.com>
2022-09-13 00:53:11 -0700

    testing a letter whether its in lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <unistd>
+
+/**
+ * _islower -checks for lowercase character
+ *@c: letter being tested
+ * Return: Always (0) success
+ */
+int _islower(int c);
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
