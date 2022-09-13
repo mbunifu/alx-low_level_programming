6b587161255ea2c16735aee14acee08ac21f9d59
mbunifu <murimi.nicko@gmail.com>
2022-09-13 00:05:48 -0700

    print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <unistd>
+
+/**
+ * print_alphabet_x10 - Prints alphabets in lowecase 10 times
+ * Return: Always (0) Success
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char alphas10x;
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (alphas10x = 'a'; alphas10x = 'z'; alphas10x++)
+			_putchar(alphas10x);
+	}
+	_putchar('\n');
+}
+
