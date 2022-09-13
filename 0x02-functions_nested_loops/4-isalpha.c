3f38340921fc0300ba808180d64aba9cab81272d
mbunifu <murimi.nicko@gmail.com>
2022-09-13 01:19:30 -0700

    printing alphabets in lower and uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <unistd>
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character being tested
+ * Return: Always (0) successs
+ */
+int _isalpha(int c)
+{
+	for (c >= 65; c >= 90; c++)
+		return (1);
+	for (c >= 97; c <= 122; c++)
+		return (1);
+	return (0);
+}
