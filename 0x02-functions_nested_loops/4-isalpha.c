c6fbe811b9e11308bc70ea70d6f625d8128e6457
mbunifu <murimi.nicko@gmail.com>
2022-09-13 05:17:17 -0700

    changing from for to if condition

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,8 @@
  */
 int _isalpha(int c)
 {
-	for ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
-	return (0);
+	else
+		return (0);
 }
