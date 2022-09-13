13d3a09d43e337a23b2505d39caad540432c7472
mbunifu <murimi.nicko@gmail.com>
2022-09-13 06:41:17 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ * @n: the number being tested
+ * Return: Always 0 success
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	if (n < 0)
+		n = -n;
+	_putchar(n + '0');
+	return (0);
+}
