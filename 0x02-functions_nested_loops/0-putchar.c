ce212dba74e99e3d525ab3a2156fc5340bba86e8
mbunifu <murimi.nicko@gmail.com>
2022-09-12 23:30:28 -0700

    printing each character at a time

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - prints 0-putchar
+ * Return: On success 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project involves c loops and functions
