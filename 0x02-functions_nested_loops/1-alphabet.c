4b3721fe30a47318c417959d64e42efc6f430a86
mbunifu <murimi.nicko@gmail.com>
2022-09-12 23:50:21 -0700

    printing the alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: Always (0) Success
+ *
+ */
+void print_alphabet(void)
+{
+	char lowers;
+
+	for (lowers = 'a'; lowers <= 'z'; lowers++)
+		_putchar(lowers);
+	_putchar('\n');
+}
