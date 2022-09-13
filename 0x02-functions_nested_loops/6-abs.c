f9ae1398122d97decffd86537e105fb9bdeb008d
mbunifu <murimi.nicko@gmail.com>
2022-09-13 06:24:17 -0700

    correcting betty stlye errors'

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * abs - computes the absolute value of an integer
+ * _abs - computes the absolute value of an integer
  * Return: (0) success
  * @a: number being tested
  */
-int _abs (int a)
+int _abs(int a)
 {
-	if ( a < 0 )
+	if (a < 0)
 		return (-a);
 	else
 		return (a);
