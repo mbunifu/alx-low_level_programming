24dd2df83d37b456f0bef43107ff6aa07e94f041
mbunifu <murimi.nicko@gmail.com>
2022-09-13 08:51:11 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - prints x by x muiltiplication table
+ * Return: Always 0 success
+ */
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			result = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (j == 0)
+				_putchar('0');
+			else if (result >= 10)
+			{
+				_putchar((result / 10) + '0');
+				_putchar(result % 10 + '0');
+			}
+			else if ((result < 10) && (j != 0))
+			{
+				_putchar(' ');
+				_putchar(result % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
