ac2263f18808b40de9a2303d0b28c665ef8b5818
mbunifu <murimi.nicko@gmail.com>
2022-09-22 06:18:59 -0700

    fixed errors

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,12 +13,11 @@ char *_strcat(char *dest, char *src)
 	int b;
 
 	for (a = 0; dest[a] != '\0'; a++)
-
+		;
 	for (b = 0; src[b] != '\0'; b++)
 	{
 		dest[a] = src[b];
 		a++;
-		b++;
 	}
 	return (dest);
 }
