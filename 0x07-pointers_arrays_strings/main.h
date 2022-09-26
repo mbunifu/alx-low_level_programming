4c5276f4e442dc00be15ab044c12efb2a2201d1e
mbunifu <murimi.nicko@gmail.com>
2022-09-26 13:44:24 -0700

    fixed error

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
-#ifdef MAIN_H_
-#define MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -9,7 +8,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
+int _putchar(char c);
 void set_string(char **s, char *to);
 
 #endif /* MAin_H_ */
