c3f6096938e6d7359d1a7f2550ee04b74207e12e
mbunifu <murimi.nicko@gmail.com>
2022-10-10 08:42:15 -0700

    fixed error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - this is a satruct called dog
@@ -13,3 +14,11 @@ struct dog
 	float age;
 	char *owner;
 }
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
