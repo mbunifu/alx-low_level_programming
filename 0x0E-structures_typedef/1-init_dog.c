4a4524e6924d5c587ee321534282e61d8373bf21
mbunifu <murimi.nicko@gmail.com>
2022-10-10 09:06:23 -0700

    initial commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - main function
+ * @d: the dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d != NULL)
+	{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+	}
+}
