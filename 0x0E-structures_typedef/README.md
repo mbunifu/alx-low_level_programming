58e8a955c653d8a3436655028c58e3b50df40918
mbunifu <murimi.nicko@gmail.com>
2022-10-10 08:35:24 -0700

    initial commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this Project is about structutres and typedef in c programming
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * struct dog - this is a satruct called dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+}
