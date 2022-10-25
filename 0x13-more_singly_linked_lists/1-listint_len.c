f0cd97f5fdb747acf93a613f3f13e4fde3363b56
mbunifu <murimi.nicko@gmail.com>
2022-10-25 11:44:19 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - Find the length of a list
+ * @h: The pointer to the start of the list
+ *
+ * eturn: Number of nodes in the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nodecount;
+
+	nodecount = 0;
+	while (h != NULL)
+	{
+		nodecount++;
+		h = h->next;
+	}
+	return (nodecount);
+}
